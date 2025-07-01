e665f66043e940fe010d45d6fae7e85e1b67b0dc
npapageorgopoulos <npapageorgopoulos@ubitech.eu>
2025-07-01 10:28:17 +0300

    OPA Gatekeeper Installation script updates

--- OPA-GATEKEEPER-INSTALL.sh
+++ OPA-GATEKEEPER-INSTALL.sh
@@ -1,22 +1,23 @@
-# Define the repository URL as an environment variable
+#!/bin/bash
+
+$dau bash -c '
+
+# Grant cluster-admin permissions to the current kubectl user
+kubectl create clusterrolebinding cluster-admin-binding \
+  --clusterrole=cluster-admin \
+  --user="$USER"
+
+# Set the location of your constraint templates
 REPO_URL="https://raw.githubusercontent.com/eu-nebulous/security-manager/dev/opa-gatekeeper-library"
 
-# Cluster admin permissions binding before the installation
-kubectl create clusterrolebinding cluster-admin-binding \
-    --clusterrole cluster-admin \
-    --user $USER
-
-# Deploy a released version of Gatekeeper in cluster with a prebuilt image
+# Install Gatekeeper
 kubectl apply -f https://raw.githubusercontent.com/open-policy-agent/gatekeeper/v3.16.3/deploy/gatekeeper.yaml
 
-# Allowed repos custom constraint template install for pods and deployments
+echo "Applying custom constraint templates..."
 kubectl apply -f "$REPO_URL/k8sallowedrepos.yaml"
-
-# Disallowed repos custom constraint template install for pods and deployments
 kubectl apply -f "$REPO_URL/k8sdisallowedrepos.yaml"
-
-# Requires that objects with the field spec.replicas specify a number of replicas within defined ranges
 kubectl apply -f "$REPO_URL/k8sreplicalimits.yaml"
-
-# Requires container images to have an image tag different from the ones in the specified list
 kubectl apply -f "$REPO_URL/k8sdisallowedtags.yaml"
+'
+
+echo "OPA Gatekeeper installation completed."
